659890c466ec0aacc64d52573c1e7dffc15982cb
huaxiaoyu42 <448898123@qq.com>
2021-03-05 21:55:42 +0800

    ^_^

--- chapter2-LinearList/src/linkedlist.c
+++ chapter2-LinearList/src/linkedlist.c
@@ -18,6 +18,53 @@ bool InitList(LNode **L){
     return true;
 }
 
+bool ListInsert(LNode *L,int i, ElemType e){
+    if(i<1){
+        return false;
+    }
+    LNode *p;
+    int j = 0;
+    p = L;
+    while(p != NULL && j < i - 1){
+        p = p->next;
+        j++;
+    }
+    if(p == NULL){
+        return false;
+    }
+    LNode *s = (LNode *)malloc(sizeof(LNode));
+    s->data = e;
+    s->next = p->next;
+    p->next = s;
+    return true;
+}
+    
+bool InsertNextNode(LNode *L, ElemType e){
+    if(L == NULL){
+        return false;
+    }
+    LNode *s = (LNode *)malloc(sizeof(ElemType));
+    s->data = e;
+    s->next = L->next;
+    L->next = s;
+    return true;
+}
+
+bool InsertPriorNode(LNode *L, ElemType e){
+    if(L == NULL){
+        return false;
+    }
+    LNode *s = (LNode *)malloc(sizeof(LNode));
+    if(s == NULL){
+        return false;
+    }
+    s->next = L->next;
+    L->next = s;
+    s->data = L->data;
+    L->data = e;
+    return true;
+}
+
 int main(void){
     LNode *L;
     InitList(&L);
