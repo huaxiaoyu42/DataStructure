dc7d71c6db25ab77e9a84ffb0fe972d961449df4
huaxiaoyu42 <448898123@qq.com>
2021-03-25 23:10:39 +0800

    写了移动数组，但是跑不起来

--- test/sqlist_test.c
+++ test/sqlist_test.c
@@ -3,16 +3,32 @@
 
 int main(){
 
-    int a[10]={1,2,3,4,5,6,7,8,9,10};
+    // int a[10]={1,2,3,4,5,6,7,8,9};
 
 
+    SqList *l;
+    InitList(l);
+
+    l->data[0] = 0;
+    l->data[1] = 1;
+    l->data[2] = 2;
+    l->data[3] = 3;
+    l->data[4] = 4;
+    l->data[5] = 5;
+    l->data[6] = 6;
+    l->data[7] = 7;
+    l->data[8] = 8;
+    l->data[9] = 9;
+
+    l->length = 10;
 
 
+    // Reverse_two_list(a,5,5,10);
+    MoveList(l,2);
 
-    Reverse_two_list(a,5,5,10);
 
     for(int i = 0;i<10;i++){
-        printf("%d ",a[i]);
+        printf("%d ",l->data[i]);
     }
 
     return 0;
