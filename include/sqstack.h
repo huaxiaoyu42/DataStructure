be470632c165218a04f363a84f8fa70576aa7796
huaxiaoyu42 <448898123@qq.com>
2021-03-27 22:53:45 +0800

    栈开了个头

---
+++ include/sqstack.h
@@ -0,0 +1,23 @@
+#ifndef SQSTACK_H
+#define SQSTACK_H
+
+#include<stdbool.h>
+#define MaxSize 10
+
+
+#define ElemType int
+
+typedef struct {
+    ElemType data[MaxSize];
+    int top;
+}SqStack;
+
+void InitStack(SqStack *s);
+
+bool Push(SqStack *s,ElemType x);
+
+bool Pop(SqStack *s,ElemType *x);
+
+bool GetTop(SqStack *s,ElemType *x);
+
+#endif
---
+++ src/sqstack.c
@@ -0,0 +1,33 @@
+#include "../include/sqstack.h"
+
+
+void InitStack(SqStack *s){
+    s->top = -1;
+}
+
+bool Push(SqStack *s,ElemType x){
+    if(s->top == MaxSize-1){
+        return false;
+    }
+    s->top = s->top + 1;
+    s->data[s->top] = x;
+    return true;
+}
+
+bool Pop(SqStack *s,ElemType *x){
+    if(s->top == -1){
+        return false;
+    }
+    *x = s->data[s->top];
+    s->top = s->top - 1;
+    return true;
+}
+
+bool GetTop(SqStack *s,ElemType *x){
+    if(s->top == -1){
+        return false;
+    }
+    *x = s->data[s->top];
+    
+    return true;
+}
